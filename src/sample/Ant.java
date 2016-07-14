a5a7081339b5cee97b5a99e943f339fed03d698d
willriggins <williamriggins2013@gmail.com>
2016-07-14 10:23:28 -0400

    draw ants

---
+++ src/sample/Ant.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by will on 7/14/16.
+ */
+public class Ant {
+    double x;
+    double y;
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,50 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main extends Application {
+    static final int HEIGHT = 600;
+    static final int WIDTH = 800;
+    static final int ANT_COUNT = 100;
+
+    static ArrayList<Ant> ants = new ArrayList<>();
+
+    static void createAnts() {
+        for (int i = 0; i < ANT_COUNT; i++) {
+            Random r = new Random();
+            Ant a = new Ant(r.nextInt(WIDTH), r.nextInt(HEIGHT));
+            ants.add(a);
+        }
+    }
+
+    static void drawAnts(GraphicsContext context) {
+        context.clearRect(0, 0, WIDTH, HEIGHT);
+        for (Ant ant : ants) {
+            context.setFill(Color.BLACK);
+            context.fillOval(ant.x, ant.y, 5, 5);
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Ants");
+        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
         primaryStage.show();
+
+        Canvas canvas = (Canvas) primaryStage.getScene().lookup("#canvas");
+        GraphicsContext context = canvas.getGraphicsContext2D();
+
+        createAnts();
+        drawAnts(context);
+
     }
 
 
